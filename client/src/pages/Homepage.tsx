c007d270badb63efc2f19c108babe6ee9d3bff83
Adam Billeqvist <adam.billeqvist@outlook.com>
2022-09-21 16:02:47 +0200

    Moment 7, Adding Show Courses component

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,13 +1,33 @@
-import React from "react";
-import Courses from "../components/Courses";
+import React, { useState, useEffect } from "react";
+import { Row } from "antd";
+import agent from "../actions/agent";
+import { PaginatedCourse } from "../models/paginatedCourse";
+import { Course } from "../models/course";
+import ShowCourses from "../components/ShowCourses";
 
 const Homepage = () => {
-    return <h1>
-      <div>
-        <Courses/>
+  const [data, setData] = useState<PaginatedCourse>();
 
+  useEffect(() => {
+    agent.Courses.list().then((response) => {
+      setData(response);
+    });
+  }, []);
+
+  return (
+    <div className="course">
+      <div className="course__header">
+        <h1>What to learn Next?</h1>
+        <h2>New Courses picked just for you...</h2>
       </div>
-    </h1>;
-  };
-  
-  export default Homepage;
+      <Row gutter={[24, 32]}>
+        {data &&
+          data.data.map((course: Course, index: number) => {
+            return <ShowCourses key={index} course={course} />;
+          })}
+      </Row>
+    </div>
+  );
+};
+
+export default Homepage;
