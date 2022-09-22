ea2bf37b5ab57cbbf815bce397de9acf72fc85cb
Adam Billeqvist <adam.billeqvist@outlook.com>
2022-09-22 17:34:56 +0200

    Adding search filters to homepage

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,17 +1,34 @@
-import React, { useEffect } from "react";
-import { Row } from "antd";
-import { Course } from "../models/course";
-import ShowCourses from "../components/ShowCourses";
-import { useAppDispatch, useAppSelector } from "../redux/store/configureStore";
-import { coursesSelector, getCoursesAsync } from "../redux/slice/courseSlice";
+import React, { useEffect } from 'react';
+import { Card, Col, Radio, Row } from 'antd';
+import { Course } from '../models/course';
+import ShowCourses from '../components/ShowCourses';
+import { useAppDispatch, useAppSelector } from '../redux/store/configureStore';
+import { coursesSelector, getCoursesAsync } from '../redux/slice/courseSlice';
+import { Category } from '../models/category';
+import { categoriesSelector } from '../redux/slice/categorySlice';
+
+const sortOptions = [
+  { value: 'title', label: 'Alphabetical' },
+  { value: 'priceDescending', label: 'Price - High to low' },
+  { value: 'priceAscending', label: 'Price - Low to high' },
+];
 
 const Homepage = () => {
-const courses = useAppSelector(coursesSelector.selectAll)
-const dispatch = useAppDispatch()
-const {coursesLoaded} = useAppSelector(state => state.course)
+  const data = useAppSelector(coursesSelector.selectAll);
+  const dispatch = useAppDispatch();
+  const { coursesLoaded } = useAppSelector((state) => state.course);
+  const categories = useAppSelector(categoriesSelector.selectAll);
+
+  const getCategories = () => {
+    const catArray: any[] = [];
+    categories.forEach((category: Category) => {
+      catArray.push({ value: category.id, label: category.name });
+    });
+    return catArray;
+  };
 
   useEffect(() => {
-    if(!coursesLoaded) dispatch(getCoursesAsync())
+    if (!coursesLoaded) dispatch(getCoursesAsync());
   }, [coursesLoaded, dispatch]);
 
   return (
@@ -21,10 +38,22 @@ const {coursesLoaded} = useAppSelector(state => state.course)
         <h2>New Courses picked just for you...</h2>
       </div>
       <Row gutter={[24, 32]}>
-        {courses &&
-          courses.map((course: Course, index: number) => {
-            return <ShowCourses key={index} course={course} />;
-          })}
+        <Col span={4}>
+          <Card title="Sorting Options">
+            <Radio.Group options={sortOptions} />
+          </Card>
+          <Card title="Choose Category">
+            <Radio.Group options={getCategories()} />
+          </Card>
+        </Col>
+        <Col span={20}>
+          <Row gutter={[24, 32]}>
+            {data &&
+              data.map((course: Course, index: number) => {
+                return <ShowCourses key={index} course={course} />;
+              })}
+          </Row>
+        </Col>
       </Row>
     </div>
   );
