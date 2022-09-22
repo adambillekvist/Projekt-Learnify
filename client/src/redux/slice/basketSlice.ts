983bdada89a08246635e20dd983b97ed6f3e5cf9
Adam Billeqvist <adam.billeqvist@outlook.com>
2022-09-22 15:44:39 +0200

    basketSlice asynchronous calls with Redux

--- client/src/redux/slice/basketSlice.ts
+++ client/src/redux/slice/basketSlice.ts
@@ -1,16 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { Basket } from '../../models/basket';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import agent from "../../actions/agent";
+import { Basket } from "../../models/basket";
 
 interface BasketState {
-  basket: Basket | null;
+  basket: Basket | null | undefined;
+  status: string;
 }
 
 const initialState: BasketState = {
   basket: null,
+  status: "idle",
 };
 
+export const addBasketItemAsync = createAsyncThunk<
+  Basket | undefined,
+  { courseId: string }
+>("basket/addBasketItemAsync", async ({ courseId }) => {
+  try {
+    return await agent.Baskets.addItem(courseId);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 export const basketSlice = createSlice({
-  name: 'basket',
+  name: "basket",
   initialState,
   reducers: {
     setBasket: (state, action) => {
@@ -19,11 +33,23 @@ export const basketSlice = createSlice({
     removeItem: (state, action) => {
       const { courseId } = action.payload;
       const itemIndex = state.basket?.items.findIndex(
-        (i) => i.courseId === courseId,
+        (i) => i.courseId === courseId
       );
       if (itemIndex === undefined || itemIndex === -1) return;
       state.basket?.items.splice(itemIndex, 1);
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(addBasketItemAsync.pending, (state) => {
+      state.status = "pending";
+    });
+    builder.addCase(addBasketItemAsync.fulfilled, (state, action) => {
+      state.basket = action.payload;
+      state.status = "idle";
+    });
+    builder.addCase(addBasketItemAsync.rejected, (state) => {
+      state.status = "idle";
+    });
   },
 });
 
