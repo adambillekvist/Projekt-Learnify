9b856486740d6f4d029215518e0ba73ce755e397
Adam Billeqvist <adam.billeqvist@outlook.com>
2022-09-22 16:34:18 +0200

    Implementing entity adapters

---
+++ client/src/redux/slice/courseSlice.ts
@@ -0,0 +1,43 @@
+import {
+    createAsyncThunk,
+    createEntityAdapter,
+    createSlice,
+  } from "@reduxjs/toolkit";
+  import agent from "../../actions/agent";
+  import { Course } from "../../models/course";
+  import { PaginatedCourse } from "../../models/paginatedCourse";
+  
+  const coursesAdapter = createEntityAdapter<Course>();
+  
+  export const getCoursesAsync = createAsyncThunk<
+    PaginatedCourse | undefined,
+    void
+  >("course/getCoursesAsync", async () => {
+    try {
+      return await agent.Courses.list();
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  
+  export const courseSlice = createSlice({
+    name: "course",
+    initialState: coursesAdapter.getInitialState({
+      coursesLoaded: false,
+      status: "idle",
+    }),
+    reducers: {},
+    extraReducers: (builder) => {
+      builder.addCase(getCoursesAsync.pending, (state) => {
+        state.status = "pending";
+      });
+      builder.addCase(getCoursesAsync.fulfilled, (state, action) => {
+        coursesAdapter.setMany(state, action.payload!.data);
+        state.status = "idle";
+        state.coursesLoaded = true;
+      });
+      builder.addCase(getCoursesAsync.rejected, (state) => {
+        state.status = "idle";
+      });
+    },
+  });
