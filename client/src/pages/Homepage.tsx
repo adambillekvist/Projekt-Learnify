0b77cbe8cf44b17bf9536b3b398be11e1734960b
Adam Billeqvist <adam.billeqvist@outlook.com>
2022-09-22 18:24:20 +0200

    Implementing Frontend Pagination

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import { Card, Col, Radio, Row } from 'antd';
 import { Course } from '../models/course';
 import ShowCourses from '../components/ShowCourses';
+import { Pagination } from 'antd';
 import { useAppDispatch, useAppSelector } from '../redux/store/configureStore';
 import {
   coursesSelector,
   getCoursesAsync,
   setCourseParams,
+  setPageNumber,
 } from '../redux/slice/courseSlice';
 import { categoriesSelector } from '../redux/slice/categorySlice';
 import { Category } from '../models/category';
@@ -20,7 +22,7 @@ const sortOptions = [
 const Homepage = () => {
   const data = useAppSelector(coursesSelector.selectAll);
   const dispatch = useAppDispatch();
-  const { coursesLoaded, courseParams } = useAppSelector(
+  const { coursesLoaded, pagination, courseParams } = useAppSelector(
     (state) => state.course,
   );
   const categories = useAppSelector(categoriesSelector.selectAll);
@@ -37,13 +39,16 @@ const Homepage = () => {
     if (!coursesLoaded) dispatch(getCoursesAsync());
   }, [coursesLoaded, dispatch]);
 
+  function onChange(pageNumber: number) {
+    dispatch(setPageNumber({ pageIndex: pageNumber }));
+  }
+
   return (
     <div className="course">
       <div className="course__header">
         <h1>What to learn Next?</h1>
         <h2>New Courses picked just for you...</h2>
       </div>
-      
       <Row gutter={[24, 32]}>
         <Col span={4}>
           <Card title="Sorting Options">
@@ -69,14 +74,19 @@ const Homepage = () => {
           <Row gutter={[24, 32]}>
             {data &&
               data.map((course: Course, index: number) => {
-                return (
-                  <ShowCourses
-                    key={index}
-                    course={course}
-                  />
-                );
+                return <ShowCourses key={index} course={course} />;
               })}
           </Row>
+          <div className="pagination">
+            {pagination && (
+              <Pagination
+                defaultCurrent={pagination?.pageIndex}
+                total={pagination?.totalCount}
+                onChange={onChange}
+                pageSize={pagination?.pageSize}
+              />
+            )}
+          </div>
         </Col>
       </Row>
     </div>
--- client/src/redux/slice/courseSlice.ts
+++ client/src/redux/slice/courseSlice.ts
@@ -2,106 +2,134 @@ import {
   createAsyncThunk,
   createEntityAdapter,
   createSlice,
-} from '@reduxjs/toolkit'
-import agent from '../../actions/agent'
-import { Course, CourseParams } from '../../models/course'
-import { PaginatedCourse } from '../../models/paginatedCourse'
-import { RootState } from '../store/configureStore'
+} from "@reduxjs/toolkit";
+import agent from "../../actions/agent";
+import { Course, CourseParams } from "../../models/course";
+import { PaginatedCourse } from "../../models/paginatedCourse";
+import { Pagination } from "../../models/pagination";
+import { RootState } from "../store/configureStore";
 
-const coursesAdapter = createEntityAdapter<Course>()
+interface CourseState {
+  coursesLoaded: boolean;
+  status: string;
+  pagination: Pagination | null;
+  courseParams: CourseParams;
+}
+
+const coursesAdapter = createEntityAdapter<Course>();
 
 function getAxiosParams(courseParams: CourseParams) {
-  const params = new URLSearchParams()
-  params.append('pageIndex', courseParams.pageIndex.toString())
-  params.append('pageSize', courseParams.pageSize.toString())
-  params.append('sort', courseParams.sort)
+  const params = new URLSearchParams();
+  params.append("pageIndex", courseParams.pageIndex.toString());
+  params.append("pageSize", courseParams.pageSize.toString());
+  params.append("sort", courseParams.sort);
   if (courseParams.category)
-    params.append('categoryId', courseParams.category.toString())
-  if (courseParams.search) params.append('search', courseParams.search)
-  return params
+    params.append("categoryId", courseParams.category.toString());
+  if (courseParams.search) params.append("search", courseParams.search);
+  return params;
 }
 
 export const getCoursesAsync = createAsyncThunk<
   PaginatedCourse | undefined,
   void,
   { state: RootState }
->('course/getCoursesAsync', async (_, thunkAPI) => {
-  const params = getAxiosParams(thunkAPI.getState().course.courseParams)
+>("course/getCoursesAsync", async (_, thunkAPI) => {
+  const params = getAxiosParams(thunkAPI.getState().course.courseParams);
   try {
-    const response = await agent.Courses.list(params)
-    return response
-  } catch (err) {
-    console.log(err)
+    const response = await agent.Courses.list(params);
+    const paged = {
+      pageIndex: response.pageIndex,
+      pageSize: response.pageSize,
+      totalCount: response.count,
+    };
+    thunkAPI.dispatch(setPagination(paged));
+    return response;
+  } catch (err: any) {
+    console.log(err);
   }
-})
+});
 
 export const getCourseAsync = createAsyncThunk<
   Course | undefined,
   { courseId: string }
->('course/getCourseAsync', async ({ courseId }, thunkAPI) => {
+>("course/getCourseAsync", async ({ courseId }, thunkAPI) => {
   try {
-    return await agent.Courses.getById(courseId)
-  } catch (err) {
-    console.log(err)
+    return await agent.Courses.getById(courseId);
+  } catch (err: any) {
+    console.log(err);
   }
-})
+});
 
 function getParams() {
   return {
     pageIndex: 1,
     pageSize: 3,
-    sort: 'title',
-  }
+    sort: "title",
+  };
 }
 
 export const courseSlice = createSlice({
-  name: 'course',
-  initialState: coursesAdapter.getInitialState<any>({
+  name: "course",
+  initialState: coursesAdapter.getInitialState<CourseState>({
     coursesLoaded: false,
-    status: 'idle',
+    status: "idle",
+    pagination: null,
     courseParams: getParams(),
   }),
   reducers: {
     setCourseParams: (state, action) => {
-      state.coursesLoaded = false
+      state.coursesLoaded = false;
       state.courseParams = {
         ...state.courseParams,
         ...action.payload,
         pageIndex: 1,
-      }
+      };
+    },
+    setPageNumber: (state, action) => {
+      state.coursesLoaded = false;
+      state.courseParams = { ...state.courseParams, ...action.payload };
     },
     setPagination: (state, action) => {
-      state.pagination = action.payload
+      state.pagination = action.payload;
+    },
+    resetCourseParams: (state) => {
+      state.courseParams = getParams();
     },
   },
   extraReducers: (builder) => {
     builder.addCase(getCoursesAsync.pending, (state) => {
-      state.status = 'pendingCourses'
-    })
+      state.status = "pendingCourses";
+    });
     builder.addCase(getCoursesAsync.fulfilled, (state, action) => {
-      coursesAdapter.setAll(state, action.payload!.data)
-      state.status = 'idle'
-      state.coursesLoaded = true
-    })
+      coursesAdapter.setAll(state, action.payload!.data);
+      state.status = "idle";
+      state.coursesLoaded = true;
+    });
     builder.addCase(getCoursesAsync.rejected, (state) => {
-      state.status = 'idle'
-    })
+      state.status = "idle";
+    });
     builder.addCase(getCourseAsync.pending, (state) => {
-      state.status = 'pendingCourse'
-    })
+      state.status = "pendingCourse";
+    });
     builder.addCase(getCourseAsync.fulfilled, (state, action) => {
-      coursesAdapter.upsertOne(state, action.payload!)
-      state.status = 'idle'
-    })
+      coursesAdapter.upsertOne(state, action.payload!);
+      state.status = "idle";
+    });
     builder.addCase(getCourseAsync.rejected, (state, action) => {
-      console.log(action.payload)
-      state.status = 'idle'
-    })
+      console.log(action.payload);
+      state.status = "idle";
+    });
   },
-})
+});
 
 export const coursesSelector = coursesAdapter.getSelectors(
-  (state: RootState) => state.course,
-)
+  (state: RootState) => state.course
+);
 
-export const { setCourseParams } = courseSlice.actions
+export const {
+  setPagination,
+  setPageNumber,
+  setCourseParams,
+  resetCourseParams,
+
+} = courseSlice.actions;
