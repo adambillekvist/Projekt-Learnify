bea6ca1ea5242f589d69706b974c13d36244d9f6
Adam Billeqvist <adam.billeqvist@outlook.com>
2022-09-25 15:21:43 +0200

    Setting up ASP identity

--- Infrastructure/StoreContext.cs
+++ Infrastructure/StoreContext.cs
@@ -1,29 +1,38 @@
 using System.Reflection;
 using Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure
 {
-    public class StoreContext : DbContext
+    public class StoreContext : IdentityDbContext<User>
     {
-        public StoreContext(DbContextOptions options) : base(options)
-        {           
+        public StoreContext(DbContextOptions options) :
+            base(options)
+        {
         }
 
-        public DbSet<Course> Courses {get; set;}
+        public DbSet<Course> Courses { get; set; }
 
-        public DbSet<Category> Categories {get; set;}
-        public DbSet<Requirement> Requirements {get; set;}
-        public DbSet<Learning> Learnings {get; set;}
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Requirement> Requirements { get; set; }
+
+        public DbSet<Learning> Learnings { get; set; }
 
         public DbSet<Basket> Basket {get; set;}
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-
-            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            builder
+                .ApplyConfigurationsFromAssembly(Assembly
+                    .GetExecutingAssembly());
+            builder.Entity<IdentityRole>()
+            .HasData(
+                new IdentityRole{Name = "Student", NormalizedName = "STUDENT"},
+                new IdentityRole{Name = "Instructor", NormalizedName = "INSTRUCTOR"});
         }
-
     }
 }
