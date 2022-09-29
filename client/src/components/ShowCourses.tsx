dd61628cf7a973052f9a3579750c4ea2e2c3158f
Adam Billeqvist <adam.billeqvist@outlook.com>
2022-09-29 13:59:59 +0200

    Creating Lecture Page

--- client/src/components/ShowCourses.tsx
+++ client/src/components/ShowCourses.tsx
@@ -16,7 +16,8 @@ const ShowCourses = ({ course }: Props) => {
   const { basket } = useAppSelector((state) => state.basket);
   const dispatch = useAppDispatch();
 
-  const {userCourses} = useAppSelector((state) => state.user)
+  const {userCourses} = useAppSelector((state) => state.user);
+  const {currentLecture} = useAppSelector((state) => state.lecture);
 
   const checkWidth = (): void => {
     if (window.innerWidth > 1024) {
@@ -69,9 +70,11 @@ const ShowCourses = ({ course }: Props) => {
             <div className="course__bottom">
               <div className="course__bottom__price">{course.price}</div>
 
-              {userCourses?.find((item) => item.id === course.id) !==
+              {userCourses?.find((item: Course) => item.id === course.id) !==
               undefined ? (
-                  <div className="course__bottom__cart">Go to Course</div>
+                <Link to={`/learn/${course.id}/${currentLecture}`} >
+                <div className="course__bottom__cart">Go to Course</div>
+              </Link>
               ) : basket?.items.find((item) => item.courseId === course.id) !==
                 undefined ? (
                 <Link to="/basket">
