ae4d88da286f9f1350401b8f9aa8f8825e3843b5
Adam Billeqvist <adam.billeqvist@outlook.com>
2022-09-29 13:08:06 +0200

    Lectures Controller

---
+++ API/Dto/UserLectureDto.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace API.Dto
+{
+    public class UserLectureDto
+    {
+        public string CourseName { get; set; }
+
+        public List<SectionDto> Sections { get; set; }
+
+        public int CurrentLecture { get; set; }
+        
+    }
+}
